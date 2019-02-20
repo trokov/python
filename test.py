09516b3d51631a611a3cc51f6bb9fe7f81793d2a
Trokov <Trokov.dima@gmail.com>
2019-02-20 22:06:44 +0300

    test commit

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+#selenium test
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 driver = webdriver.Chrome("/home/dmitriy/Downloads/chromedriver")
@@ -6,5 +7,6 @@ assert "Python" in driver.title
 elem = driver.find_element_by_name("q")
 elem.send_keys("pycon")
 elem.send_keys(Keys.RETURN)
+
 assert "No results found." not in driver.page_source
 driver.close()
